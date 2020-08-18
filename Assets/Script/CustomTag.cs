1e6880b4e6afdc5a26cb14516012a528b46fcb7f
Yumin-Kim <dbals0@naver.com>
2020-08-18 23:56:12 +0900

    No VROculusModule Grab Cube and text

---
+++ Assets/Script/CustomTag.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomTag : MonoBehaviour
+{
+
+    public string[] tag;
+
+}
---
+++ Assets/VrGrabber/Scripts/VrgIDevice.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace VrGrabber
+{
+
+public enum ControllerSide
+{
+    Left,
+    Right,
+}
+
+public interface IDevice
+{
+    Vector3 GetLocalPosition(ControllerSide side);
+    Quaternion GetLocalRotation(ControllerSide side);
+    bool GetHold(ControllerSide side);
+    bool GetRelease(ControllerSide side);
+    bool GetHover(ControllerSide side);
+    bool GetClick(ControllerSide side);
+    Vector2 GetCoord(ControllerSide side);
+}
+
+}

4d1edfefb0057eaf6b959c6cf29384204528f6bf
Yumin-Kim <dbals0@naver.com>
2020-09-09 23:31:12 +0900

    Start Sub Project

---
+++ Assets/MyScript/CreateNewObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNewObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    InstanceScript newObject = null;
+    void Start()
+    {
+        GameObject[] ob2 = Resources.LoadAll<GameObject>("3DObj");
+        Debug.Log(ob2[2]);
+        Instantiate(ob2[2],this.transform);
+        Instantiate(ob2[5],this.transform);
+        //asd.transform.position = new Vector3(10f, 10f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

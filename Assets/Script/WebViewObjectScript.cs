ecc788ad8b27c605e79234dda2dffc93818db042
Yumin-Kim <dbals0@naver.com>
2020-09-18 02:03:13 +0900

    To implement WebView

---
+++ Assets/Script/WebViewObjectScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebViewObjectScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private WebViewObject webViewObject;
+    void Start()
+    {
+        startWebView();
+    }
+
+    private void startWebView()
+    {
+        string NaverUrl = "https://www.naver.com";
+        webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+        webViewObject.Init((msg) =>
+        {
+            Debug.Log(string.Format("CallForm[{0}]", msg));
+        });
+        webViewObject.LoadURL(NaverUrl);
+        webViewObject.SetVisibility(true);
+        webViewObject.SetMargins(50, 50, 50, 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Destroy(webViewObject);
+                return;
+            }
+        }
+
+
+    }
+}

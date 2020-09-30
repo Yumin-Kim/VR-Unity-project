55072396b97c72a713e90e10446830da7417253f
Yumin-Kim <dbals0@naver.com>
2020-10-01 02:12:16 +0900

    Reset VRGrabProject and Resolve compatibility between current and Webview Project

---
+++ Assets/Scripts/WebViewScipt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebViewScipt : MonoBehaviour
+{
+    private WebViewObject webViewObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartWebView();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (Application.platform == RuntimePlatform.Android) {
+        if (Input.GetKey(KeyCode.Escape))
+        {                    // 뒤 로 가 기, esc 버 튼 
+            Destroy(webViewObject);
+            return;
+        }
+        //}
+    }
+
+    public void StartWebView()
+    {
+
+        string strUrl = "https://sanctacrux.tistory.com/669";
+
+        webViewObject = (new GameObject("WebviewPanel")).AddComponent<WebViewObject>();
+        webViewObject.Init((msg) => {
+            Debug.Log(string.Format("CallFromJS[{0}]", msg));
+        });
+
+        webViewObject.LoadURL(strUrl);
+        webViewObject.SetVisibility(true);
+        webViewObject.SetMargins(50, 50, 50, 50);
+    }
+}
